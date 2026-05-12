6ad4cf6df4246f3ebb6ebcf5a05982e00064da95
ghoulson29 <jdrandazzo2002@gmail.com>
2026-05-12 11:32:02 +0200

    Redo the mean calculus to take account for the area each u represents

--- fluid/computeMeanVel.m
+++ fluid/computeMeanVel.m
@@ -1,9 +1,24 @@
-function umean = computeMeanVel(mesh,domain,solution)
+function umean = computeMeanVel(mesh, domain, solution)
 
-u = solution.u;
+    X = mesh.X;
+    T = mesh.T;
+    u = solution.u;
+    ux = u(:,1);  % x-velocity (left to right)
 
-%u_scalar = sqrt(sum(u.*u,2));
-u_scalar = u(:,1); % we want left to right vel
+    % Area of each triangle
+    x1 = X(T(:,1),1); y1 = X(T(:,1),2);
+    x2 = X(T(:,2),1); y2 = X(T(:,2),2);
+    x3 = X(T(:,3),1); y3 = X(T(:,3),2);
 
-umean = mean(u_scalar);
+    areas = 0.5 * abs((x2-x1).*(y3-y1) - (x3-x1).*(y2-y1));
 
+    % Average ux at each triangle centroid
+    ux_elem = (ux(T(:,1)) + ux(T(:,2)) + ux(T(:,3))) / 3;
+
+    % Total domain area from domain extents (already known)
+    A_total = ((domain.x_LR - domain.x_LL) * (domain.y_LU - domain.y_LD))*10^(-12);
+
+    % Area-weighted integral divided by total area
+    umean = sum(areas .* ux_elem) / A_total;
+
+end
